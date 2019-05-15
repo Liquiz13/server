38e5433a5b94a720724f6a754253922efa8b8ced
Liquiz13 <37296282+Liquiz13@users.noreply.github.com>
2019-05-15 17:05:21 +0300

    Add files via upload

--- server/app.js
+++ server/app.js
@@ -1,13 +1,35 @@
-const http = require('http')
-const port = 3000
-const requestHandler = (request, response) => {
-    console.log(request.url)
-    response.end('server working now!')
-}
-const server = http.createServer(requestHandler)
-server.listen(port, (err) => {
-    if (err) {
-        return console.log('something bad happened', err)
-    }
-    console.log(`server is listening on ${port}`)
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+const users = []
+app.post('/users', function (req, res) {
+    const user = req.body
+    users.push({
+        name: user.name,
+        age: user.age
+    })
+    res.send('successfully registered')
 })
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
+
+app.get('/', function (req, res) {
+    res.send('GET request to the homepage');
+});
+
+app.post('/', function (req, res) {
+    res.send('POST request to the homepage');
+});
+
+app.delete('/', function (req, res) {
+    res.send('Got a DELETE request at /user');
+});
+
+app.put('/', function (req, res) {
+    res.send('Got a PUT request at /user');
+});
